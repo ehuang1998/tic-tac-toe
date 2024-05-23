2456e1aea9b16bf3e88439c866a6f342e887b128
Eric <16390263+ehuang1998@users.noreply.github.com>
2024-05-22 22:45:10 -0400

    created gameboard functionality and completed css styling

--- javascript.js
+++ javascript.js
@@ -1,6 +1,53 @@
+const Gameboard = (() => {
+
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class = "square" id = square-${index}">${square}</div>`
+        })
+
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+    }
+
+    return {
+        render,
+    }
+
+})();
+
+// factory function to create players
+const createPlayer = function (name, mark) {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, "X"),
+            createPlayer(document.querySelector('#player2').value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+
+    return {
+        start,
+    }
+
+})();
+
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener('click', () => {
-    alert("Hello World");
-    
-    console.log("Hello World!");
+    Game.start();
 })
